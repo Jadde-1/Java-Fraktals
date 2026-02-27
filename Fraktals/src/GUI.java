ed41dee240f012ca71969621e824bd94db983350
Jadde <47781581+Jadde-1@users.noreply.github.com>
2026-02-27 08:55:08 +0100

    New class

--- Fraktals/src/GUI.java
+++ Fraktals/src/GUI.java
@@ -1,9 +1,16 @@
+import javax.swing.JFrame;
 
 public class GUI {
 
-	public static void main(String[] args) {
-		//todo
-
+	GUI() {
+		System.out.println("printer 1");
+		
+		//kreation af the frame
+				JFrame mainFrame = new JFrame("A* finding");
+				mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				PathPanel pathPanel = new PathPanel();
+				mainFrame.add(pathPanel);
+				mainFrame.pack();
+				mainFrame.setVisible(true);
 	}
-
 }
---
+++ Fraktals/src/PathPanel.java
@@ -0,0 +1,34 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.util.ArrayList;
+import java.util.Random;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
+//-----------imports------------------
+public class PathPanel extends JPanel {
+
+    
+
+
+
+
+    //font for h g og f
+    private Font font = new Font("Arial Black", Font.BOLD, 12);
+    
+    public PathPanel() {
+        setPreferredSize(new Dimension(800, 600));
+        setFocusable(true);
+
+
+        //Idea Make a way to
+       /* addMouseListener(new MouseAdapter() {
+            public void mousePressed(MouseEvent e) {
+
+                }
+            }
+        });
+        */
+
+    }
+}
